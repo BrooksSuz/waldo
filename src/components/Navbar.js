c9c31024cbff3a33bfa16d3fc2c6051371446112
Brooks Susor <susorbrooks@gmail.com>
2022-11-23 12:51:49 -0500

    Create timer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,28 @@
+import { useEffect, useState } from "react";
 import "../styles/Navbar.css";
 
 const Navbar = () => {
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(null);
+
+  useEffect(() => {
+    const interval = setInterval(() => setSeconds(seconds + 1), 1000);
+    return () => {
+      if (seconds === 59) {
+        setSeconds(0);
+        setMinutes(minutes + 1);
+        clearInterval(interval);
+      } else {
+        clearInterval(interval);
+      }
+    };
+  }, [seconds, minutes]);
+
   return (
     <nav>
       <ul>
         <li><h1>Restart</h1></li>
-        <li><h1>Timer: </h1></li>
+        <li><h1>{minutes} : {seconds}</h1></li>
       </ul>
     </nav>
   );
