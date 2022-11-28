d10e81ecda9b50d58dd4295a29e61da628049a6e
Brooks Susor <susorbrooks@gmail.com>
2022-11-28 11:51:52 -0500

    Begin moving game logic to DropdownMenu component

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -1,5 +1,55 @@
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase-config";
+
 const DropdownMenu = (props) => {
-  const { top, left } = props;
+  const { userGuess, top, left } = props;
+
+  const locationsCollectionRef = collection(db, "waldo-locations");
+
+  const getWaldos = async () => {
+    const data = await getDocs(locationsCollectionRef);
+    return data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+  };
+
+  const getMineGuy = async () => {
+    const response = await getWaldos();
+    return response[0];
+  };
+
+  const getPolarBear = async () => {
+    const response = await getWaldos();
+    return response[1];
+  };
+
+  const getTinyCastle = async () => {
+    const response = await getWaldos();
+    return response[2];
+  };
+
+  const checkGuess = async () => {
+    const mineGuy = await getMineGuy();
+    const polarBear = await getPolarBear();
+    const tinyCastle = await getTinyCastle();
+
+    if ((userGuess[0] >= mineGuy.X[0]
+      && userGuess[0] <= mineGuy.X[1])
+      && (userGuess[1] >= mineGuy.Y[0]
+      && userGuess[1] <= mineGuy.Y[1])) {
+      console.log('You found the Mine Guy!');
+    } else if ((userGuess[0] >= polarBear.X[0]
+      && userGuess[0] <= polarBear.X[1])
+      && (userGuess[1] >= polarBear.Y[0]
+      && userGuess[1] <= polarBear.Y[1])) {
+      console.log('You found the Polar Bear!');
+    } else if ((userGuess[0] >= tinyCastle.X[0]
+      && userGuess[0] <= tinyCastle.X[1])
+      && (userGuess[1] >= tinyCastle.Y[0]
+      && userGuess[1] <= tinyCastle.Y[1])) {
+      console.log('You found the Tiny Castle!');
+    } else {
+      console.log('You no find anything :(');
+    }
+  };
 
   return (
     <span
@@ -11,9 +61,9 @@ const DropdownMenu = (props) => {
             }}
       className="dropdown-menu"
     >
-      <button>Mine Guy</button>
-      <button>Polar Bear</button>
-      <button>Tiny Castle</button>
+      <button onClick={checkGuess}>Mine Guy</button>
+      <button onClick={checkGuess}>Polar Bear</button>
+      <button onClick={checkGuess}>Tiny Castle</button>
     </span>
   );
 };
--- src/components/WaldoBoard.js
+++ src/components/WaldoBoard.js
@@ -1,10 +1,8 @@
-import "../firebase-config";
 import { useState } from "react";
-import { db } from "../firebase-config";
-import { collection, getDocs } from "firebase/firestore";
 import DropdownMenu from "./DropdownMenu";
 
 const WaldoBoard = () => {
+  const [userGuess, setUserGuess] = useState(null);
   const [top, setTop] = useState(null);
   const [left, setleft] = useState(null);
   const [showDropdown, setShowDropdown] = useState(false);
@@ -14,59 +12,11 @@ const WaldoBoard = () => {
     tinyCastle: false
   });
 
-  const locationsCollectionRef = collection(db, "waldo-locations");
-
-  const getWaldos = async () => {
-    const data = await getDocs(locationsCollectionRef);
-    return data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
-  };
-
-  const getMineGuy = async () => {
-    const response = await getWaldos();
-    return response[0];
-  };
-
-  const getPolarBear = async () => {
-    const response = await getWaldos();
-    return response[1];
-  };
-
-  const getTinyCastle = async () => {
-    const response = await getWaldos();
-    return response[2];
-  };
-
-  const checkGuess = async (userGuess) => {
-    const mineGuy = await getMineGuy();
-    const polarBear = await getPolarBear();
-    const tinyCastle = await getTinyCastle();
-
-    if ((userGuess[0] >= mineGuy.X[0]
-      && userGuess[0] <= mineGuy.X[1])
-      && (userGuess[1] >= mineGuy.Y[0]
-      && userGuess[1] <= mineGuy.Y[1])) {
-      console.log('You found the Mine Guy!');
-    } else if ((userGuess[0] >= polarBear.X[0]
-      && userGuess[0] <= polarBear.X[1])
-      && (userGuess[1] >= polarBear.Y[0]
-      && userGuess[1] <= polarBear.Y[1])) {
-      console.log('You found the Polar Bear!');
-    } else if ((userGuess[0] >= tinyCastle.X[0]
-      && userGuess[0] <= tinyCastle.X[1])
-      && (userGuess[1] >= tinyCastle.Y[0]
-      && userGuess[1] <= tinyCastle.Y[1])) {
-      console.log('You found the Tiny Castle!');
-    } else {
-      console.log('You no find anything :(');
-    }
-  };
-
   const handleClick = (e) => {
-    const userGuess = [e.pageX, e.pageY];
+    setUserGuess([e.pageX, e.pageY]);
     setTop(e.pageY);
     setleft(e.pageX);
     setShowDropdown(!showDropdown);
-    checkGuess(userGuess);
   };
 
   return (
@@ -77,7 +27,7 @@ const WaldoBoard = () => {
         className="waldo-image"
         alt='Waldo' 
       />
-      { showDropdown ? <DropdownMenu top={top} left={left} /> : null }
+      { showDropdown ? <DropdownMenu userGuess={userGuess} top={top} left={left} /> : null }
     </div>
   );
 };
